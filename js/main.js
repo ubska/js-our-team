52acbe8c1497441638297cf3e72a02eb841c2de7
Oleksandra Ubska <alessandraubska@gmail.com>
2024-06-18 14:37:03 +0200

    create teamMembers array

--- js/main.js
+++ js/main.js
@@ -6,3 +6,52 @@
 // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 // MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+// creare un array di oggetti per rappresentare i membri del team
+const teamMembers = [
+    {
+        "nome": "Wayne Barnett",
+        "role": "Founder & CEO",
+        "photo": "wayne-barnett-founder-console.jpg"
+    },
+
+
+    {
+        "nome": "Angela",
+        "role": "Chief Editor",
+        "photo": "img/angela-caroll-chief-editor.jpg"
+
+    },
+
+    {
+        "nome": "Walter Gordon",
+        "role": "Office Manager",
+        "photo": "walter-gordon-office-manager.jpg"
+
+    },
+
+    {
+        "nome": "Angela Lopez",
+        "role": "Social Media Manager",
+        "photo": "angela-lopez-social-media-manager.jpg"
+
+    },
+
+    {
+        "nome": "Scott Estrada",
+        "role": "Developer",
+        "photo": "scott-estrada-developer.jpg"
+
+    },
+
+    {
+        "nome": "Barbara Ramos",
+        "role": "Graphic Designer",
+        "photo": "barbara-ramos-graphic-designer.jpg"
+
+    },
+
+];
+
+console.log(teamMembers);
